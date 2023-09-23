5d0507b0efa1a148fee8af232346396e6cbc13bc
Jimi <jimi.vihinen@hotmail.com>
2023-09-23 13:47:41 +0300

    changed "HumTemp_print"
    added new if statements
    changed duration
    and delay to exit

--- Humidity/HumTemp_print/HumTemp_print.ino
+++ Humidity/HumTemp_print/HumTemp_print.ino
@@ -5,11 +5,12 @@ dht DHT;
 #define DHT11_PIN 2
 
 unsigned long startTime;
-unsigned long duration = 5000;
+unsigned long duration = 15000;
 
 void setup(){
   Serial.begin(9600);
   startTime = millis();
+  
 }
 
 void loop(){
@@ -18,15 +19,40 @@ void loop(){
 
    if(currentTime - startTime >= duration) {
      Serial.print("Good Bye");
-     exit(3);
+     delay(3000);
+     exit(0);
    }
-
-    int chk = DHT.read11(DHT11_PIN);
+  //checks if the temp is over 24 celsius
+   if(DHT.temperature > 24){
     Serial.print("Temperature = ");
     Serial.println(DHT.temperature);
     Serial.print("Humidity = ");
     Serial.println(DHT.humidity);
-    delay(1000);
+    Serial.print("Is warm");
+   }
+  //checks if the temp is between 22 and 24 celsius
+   if(DHT.temperature >= 22 && DHT.temperature <= 24){
+    Serial.print("Temperature = ");
+    Serial.println(DHT.temperature);
+    Serial.print("Humidity = ");
+    Serial.println(DHT.humidity);
+    Serial.print("Just good");
+   }
+  //checks if the temp is below 22 celsius
+   if(DHT.temperature < 22){
+    Serial.print("Temperature = ");
+    Serial.println(DHT.temperature);
+    Serial.print("Humidity = ");
+    Serial.println(DHT.humidity);
+    Serial.print("Is cold");
+   }
+
+    int chk = DHT.read11(DHT11_PIN);
+    
+    Serial.println("");
+    Serial.println("");
+    Serial.println("");
+    delay(2000);
 
 }
 
